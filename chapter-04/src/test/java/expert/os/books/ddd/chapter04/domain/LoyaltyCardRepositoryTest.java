63be6ff9e0da474efaa088bb12b19d6c063f0306
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-06 09:02:31 +0100

    test: include LoyaltyCardRepositoryTest
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-04/src/test/java/expert/os/books/ddd/chapter04/domain/LoyaltyCardRepositoryTest.java
+++ chapter-04/src/test/java/expert/os/books/ddd/chapter04/domain/LoyaltyCardRepositoryTest.java
@@ -1,4 +1,32 @@
 package expert.os.books.ddd.chapter04.domain;
 
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
 public class LoyaltyCardRepositoryTest {
+
+    @Container
+    public PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:13")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @Test
+    void shouldSaveAndRetrieveLoyaltyCard() {
+        String databaseName = postgresContainer.getDatabaseName();
+        String username = postgresContainer.getUsername();
+        String password = postgresContainer.getPassword();
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(databaseName).isEqualTo("testdb");
+            softly.assertThat(username).isEqualTo("test");
+            softly.assertThat(password).isEqualTo("test");
+        });
+    }
+
 }
